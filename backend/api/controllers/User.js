261a3f6de683ac71be437eb1e51ffde100206f78
prashant <prashantchouhan711@gmail.com>
2024-09-20 19:13:35 +0530

    hash updated

--- backend/api/controllers/User.js
+++ backend/api/controllers/User.js
@@ -1,17 +1,26 @@
 import User from '../models/user.js';
 
-export const updateUser = async( req, res, next)=>{
-    try {
-        const updateUser = await User.findByIdAndUpdate(
-          req.params.id,
-          {$set: req.body},
-          {new: true}
-        );
-        res.status(200).json(updateUser);
-      } catch (err) {
-       next(err)
-      }
-}
+import bcrypt from 'bcrypt';
+import User from '../models/user.js'; // Adjust the import based on your file structure
+
+export const updateUser = async (req, res, next) => {
+  try {
+    if (req.body.password) {
+      // Hash the new password
+      const salt = await bcrypt.genSalt(10);
+      req.body.password = await bcrypt.hash(req.body.password, salt);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    next(err);
+  }
+};
 
 export const deleteUser = async( req, res, next)=>{
         try {
